7f09eabcd4a57ee402ead1e40a5ddff71ffe0e24
OhWhale515 <sterlinggreen515@gmail.com>
2023-02-09 14:18:52 -0500

    enhanced fall mechs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+const gravity = 0.5
 class Player {
     constructor() {
         this.position = {
@@ -11,7 +13,7 @@ class Player {
         }
         this.velocity = {
             x: 0,
-            y: 1
+            y: 0
         }
         this.width = 30
         this.height = 30
@@ -27,6 +29,9 @@ class Player {
         this.draw()
         this.position.y += this.velocity.y
 
+        if(this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
     }
 }
 
